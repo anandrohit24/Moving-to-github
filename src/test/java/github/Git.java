78986883a5bd9a8c50f0544b2a50cda68538b50d
anand <rohitgaya2000@gmail.com>
2026-09-04 17:03:38 +0530

    push to github

---
+++ src/test/java/github/Git.java
@@ -0,0 +1,42 @@
+package github;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Git {
+	
+	WebDriver driver;
+	@BeforeClass
+	void setup()
+	{
+		 driver = new ChromeDriver();
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+	}
+
+	@Test(priority=1)
+	void fail()
+	{
+		driver.findElement(By.id("username")).sendKeys("st");
+		driver.findElement(By.id("password")).sendKeys("Password123");
+		driver.findElement(By.id("submit")).click();
+		WebElement e= driver.findElement(By.xpath("//div[@id='error']"));
+		System.out.println(e.getText());
+	}
+	@Test(priority=2)
+	void first()
+	{
+		driver.findElement(By.id("username")).sendKeys("student");
+	driver.findElement(By.id("password")).sendKeys("Password123");
+	driver.findElement(By.id("submit")).click();
+	}
+	@AfterClass
+	void tra()
+	{
+		driver.quit();
+	}
+}
